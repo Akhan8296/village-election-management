c5abdbe31d1c67d8a562f8de8e68287f8622da04
Akhan8296 <149956359+Akhan8296@users.noreply.github.com>
2026-08-09 14:04:40 +0000

    correction

--- charts/charts_all.py
+++ charts/charts_all.py
@@ -3,12 +3,15 @@ import plotly.express as px
 def create_gender_chart(df):
     fig = px.pie(df, names="GENDER", values="VOTER_COUNT", title="Gender Distribution", hole=0.3)
     fig.update_traces(textinfo="percent+label")
+    fig.update_layout(height=350, shapes=[dict(type="rect", xref="paper", yref="paper", x0=0, y0=0, x1=1, y1=1, line=dict(color="black", width=2))])
     return fig
 
 def create_age_chart(df):
     fig = px.bar(df,x="AGE_GROUP",y="VOTER_COUNT",title="Voter Age Distribution",text="VOTER_COUNT")
+    fig.update_layout(height=350, shapes=[dict(type="rect", xref="paper", yref="paper", x0=0, y0=0, x1=1, y1=1, line=dict(color="black", width=2))])
     return fig
 
 def create_house_chart(df):
     fig = px.line(df,x="HOUSE_NO",y="VOTER_COUNT",title="Voters by House Number",markers=True)
+    fig.update_layout(height=350, shapes=[dict(type="rect", xref="paper", yref="paper", x0=0, y0=0, x1=1, y1=1, line=dict(color="black", width=2))])
     return fig
