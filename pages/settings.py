d4259e052d940bfe4e6757c3a1e83b9f62c6ca70
Akhan8296 <149956359+Akhan8296@users.noreply.github.com>
2026-08-09 19:34:01 +0000

    Added bottom of side bar

--- pages/settings.py
+++ pages/settings.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("⚙️ Settings")
+st.markdown("""
+    <h2 style="font-size: 28px; margin-top: -25px; margin-bottom: 10px; font-weight: 600;"> Settings </h2>
+    """, unsafe_allow_html=True)
 st.subheader("Account")
 st.write("**Username:** user123")
 st.write("**Role:** Viewer")
