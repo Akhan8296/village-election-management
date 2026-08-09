fe0900a01406b2e416c5221d292dba7d26cf5cbf
Akhan8296 <149956359+Akhan8296@users.noreply.github.com>
2026-08-09 15:37:00 +0000

    update

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -13,15 +13,15 @@ def show_dashboard():
 
     # Create charts
     gender_fig = create_gender_chart(df_gender)
-    gender_fig.update_layout(height=250 ,margin=dict(l=10, r=10, t=50, b=30))
+    gender_fig.update_layout(height=250 ,margin=dict(l=0, r=0, t=50, b=30))
     age_fig = create_age_chart(df_age)
-    age_fig.update_layout(height=250 ,margin=dict(l=10, r=10, t=50, b=30))
+    age_fig.update_layout(height=250 ,margin=dict(l=0, r=0, t=50, b=30))
 
     house_fig = create_house_chart(df_house)
-    house_fig.update_layout(height=300 ,margin=dict(l=10, r=10, t=50, b=30))
+    house_fig.update_layout(height=300 ,margin=dict(l=0, r=0, t=50, b=30))
 
     gender_age_fig = create_gender_age_chart(df_gender_age)
-    gender_age_fig.update_layout(xaxis_title="Age Group", yaxis_title="Number of Voters", height=250 ,margin=dict(l=10, r=10, t=50, b=30))
+    gender_age_fig.update_layout(xaxis_title="Age Group", yaxis_title="Number of Voters", height=250 ,margin=dict(l=0, r=0, t=50, b=30))
 
     # Display side by side
     col1, col2 ,col3= st.columns(3)
